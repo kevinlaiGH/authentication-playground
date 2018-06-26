5579f38025342405db39a1c2c089f29146b348b0
Kevin Lai <kevin.km.lai@gmail.com>
2018-06-26 14:38:57 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Starting point
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-Parser");
+
+const morgan = require("morgan");
+const app = express();
+const router = require("./router");
+
+// App setup (middleware)
+app.use(morgan("combined"));
+app.use(bodyParser.json({ type: "*/*" }));
+router(app);
+// Server Setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log("Server listening on: ", port);
